b72cf266379b3dc970d11b38ac9ed3883adf074c
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-08 11:53:58 +0530

    auto pushhh

--- elastiCashe-redis.tf
+++ elastiCashe-redis.tf
@@ -4,7 +4,7 @@ resource "aws_elasticache_cluster" "redis" {
   node_type            = "cache.t3.small"
   num_cache_nodes      = 1
   parameter_group_name = aws_elasticache_parameter_group.redis_pg.name
-  engine_version       = "6.2"
+  engine_version       = "6.x"
   port                 = 6379
   security_group_ids   = [aws_security_group.allow_redis.id]
 }
