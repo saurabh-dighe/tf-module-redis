3c2e13805d6c893fda58e2b7676ac9c1e0bbce74
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-08 12:25:56 +0530

    auto pushhh

--- elastiCashe-redis.tf
+++ elastiCashe-redis.tf
@@ -7,6 +7,7 @@ resource "aws_elasticache_cluster" "redis" {
   engine_version       = "6.x"
   port                 = 6379
   security_group_ids   = [aws_security_group.allow_redis.id]
+  subnet_group_name    = aws_elasticache_subnet_group.redis.name 
 }
 
 resource "aws_elasticache_parameter_group" "redis_pg" {
