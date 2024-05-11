78c95b7acf3a75f7e52fd12c8b6fae8554fa1c55
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-11 14:12:11 +0530

    auto pushhh

--- elastiCashe-redis.tf
+++ elastiCashe-redis.tf
@@ -1,18 +1,18 @@
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "roboshop-${var.ENV}-redis"
-  engine               = "redis"
-  node_type            = "cache.t3.small"
-  num_cache_nodes      = 1
+  engine               = var.REDIS_ENGINE
+  node_type            = var.REDIS_NOTE_TYPE
+  num_cache_nodes      = var.REDIS_CASHE_NODE
   parameter_group_name = aws_elasticache_parameter_group.redis_pg.name
-  engine_version       = "6.0"
-  port                 = 6379
+  engine_version       = var.REDIS_ENGINE_VERSION
+  port                 = var.REDIS_PORT
   security_group_ids   = [aws_security_group.allow_redis.id]
   subnet_group_name    = aws_elasticache_subnet_group.redis.name 
 }
 
 resource "aws_elasticache_parameter_group" "redis_pg" {
   name   = "roboshop-${var.ENV}-redis-pg"
-  family = "redis6.x"
+  family = var.REDIS_FAMILY
 }
 
 # Aws subnet group for group if subnets
--- vars.tf
+++ vars.tf
@@ -1,2 +1,8 @@
 variable "ENV" {}
 
+variable "REDIS_ENGINE" {}
+variable "REDIS_NOTE_TYPE" {}
+variable "REDIS_CASHE_NODE" {}
+variable "REDIS_ENGINE_VERSION" {}
+variable "REDIS_PORT" {}
+variable "REDIS_FAMILY" {}
