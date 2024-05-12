9510ce614d96113174ff793bed8794960724918e
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-12 22:40:29 +0530

    auto pushhh

---
+++ r53-record.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "redis" {
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
+  name    = "redis-${var.ENV}"
+  type    = "CNAME"
+  ttl     = 10
+  records = [aws_elasticache_cluster.redis.0.address]
+}
